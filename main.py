50899299489810631bd10161e057757a056e3739
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-17 16:14:25 +0530

    feat: add CRUD error handling and HTTP status codes

--- main.py
+++ main.py
@@ -1,8 +1,62 @@
+
 from typing import Any
 from fastapi import FastAPI, HTTPException
 from models import products, Product
 
 app = FastAPI()
+
+
+@app.get("/products/{target_id}")
+def product_by_id(target_id: int):
+
+    for product in products:
+        if product.id == target_id:
+            return product
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"{target_id}: Product Not found"
+    )
+
+
+@app.post("/products", status_code=201)
+def add_product(product: Product):
+    products.append(product)
+
+    for existing_product in products:
+        if existing_product.id == product.id:
+            raise HTTPException(
+                status_code=409,
+                detail=f"Product with id {product.id} already exists",
+            )
+    return products
+
+
+@app.put("/products/{target_id}")
+def update_product(target_id: int, updated_product_data: Product):
+    for i, product in enumerate(products):
+        if product.id == target_id:
+            products[i] = updated_product_data
+            return products[i]
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"{target_id}: Product Not found"
+    )
+
+
+@app.delete("/products/{target_id}")
+def delete_product(target_id: int):
+
+    for i, product in enumerate(products):
+        if product.id == target_id:
+            del products[i]
+            return products
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"{target_id}: Product Not found"
+    )
 
 
 @app.get("/products")
@@ -19,62 +73,9 @@ def get_by_condition(
         category_match = True
 
         if category is not None:
-            product.category.lower() == category.lower()
+            category_match = product.category.lower() == category.lower()
 
         if price_match and category_match:
             filter_products.append(product)
 
     return filter_products
-
-
-@app.get("/products/{target_id}")
-def product_by_id(target_id: int):
-
-    for product in products:
-        if product.id == target_id:
-            return product
-
-    return products
-
-
-@app.post("/products")
-def add_product(product: Product):
-    products.append(product)
-    raise HTTPException(
-        status_code=201,
-        detail=f"product_id:{product.id} Created Successfully"
-    )
-
-
-@app.put("/products/{target_id}")
-def update_product(target_id: int, updated_product_data: Product):
-    try:
-        for i, product in enumerate(products):
-            if product.id == target_id:
-                products[i] = updated_product_data
-                return products[i]
-
-        raise HTTPException(
-            status_code=404,
-            detail=f"{target_id}: Product Not found"
-        )
-
-    except Exception as e:
-        return e
-
-
-@app.delete("/products/{target_id}")
-def delete_product(target_id: int):
-    try:
-        for i, product in enumerate(products):
-            if product.id == target_id:
-                del products[i]
-                return products
-
-        raise HTTPException(
-            status_code=404,
-            detail=f"{target_id}: Product Not found"
-        )
-
-    except Exception as e:
-        return e
